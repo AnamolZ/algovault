03b02a4ba405234ec87e7e9f5cb4d374fe21f86f
AnamolZ <anmoldkl971@gmail.com>
2026-01-02 21:44:02 +0545

    feat: implement solution for HackerRank Designer PDF Viewer problem

--- Python/HackerRank/designerPdfViewer.py
+++ Python/HackerRank/designerPdfViewer.py
@@ -1,12 +1,25 @@
 
-def designerPdfViewer(a, w):
-    indexs = [ord(i) - ord('a') for i in w]
-    return max(a[i] for i in indexs) * len(w)
+def designerPdfViewer(h, w):
+    """
+    Calculates the area of the rectangle highlighting the word.
+
+    Args:
+        h (list of int): A list of 26 integers representing the heights of each letter.
+        word (str): The word to be highlight.
+
+    Returns:
+        int: The area of the highlighted rectangle.
+    """
+    indices = [ord(i) - ord('a') for i in w]
+    return max(h[i] for i in indices) * len(w)
 
 def main():
-    a = list(map(int, input().rstrip().split()))
+    """
+    Reads heights and a word from standard input and prints the result.
+    """
+    h = list(map(int, input().rstrip().split()))
     w = input()
-    print(designerPdfViewer(a, w))
+    print(designerPdfViewer(h, w))
 
 if __name__ == '__main__':
     main()
