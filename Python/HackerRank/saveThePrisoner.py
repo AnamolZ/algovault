6089b207fca5b9cc4b562bd9c2d92fc1309a62f2
AnamolZ <anmoldkl971@gmail.com>
2026-01-07 16:36:38 +0545

    feat: add solution for HackerRank 'Save the Prisoner!' problem

--- Python/HackerRank/saveThePrisoner.py
+++ Python/HackerRank/saveThePrisoner.py
@@ -3,8 +3,10 @@ def saveThePrisoner(n, m, s):
     return ((s + m - 2) % n) + 1
 
 def main():
-    n, m, s = map(int, input().split())
-    print(saveThePrisoner(n, m, s))
+    t = int(input().strip())
+    for _ in range(t):
+        n, m, s = map(int, input().split())
+        print(saveThePrisoner(n, m, s))
 
 if __name__ == '__main__':
     main()
