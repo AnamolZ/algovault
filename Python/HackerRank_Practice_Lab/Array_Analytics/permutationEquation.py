55dc9a57af7f8201410feabc1e721cb0b2d7bc86
AnamolZ <anmoldkl971@gmail.com>
2026-01-08 13:56:23 +0545

    docs: add documentation to permutationEquation.py

--- Python/HackerRank_Practice_Lab/Array_Analytics/permutationEquation.py
+++ Python/HackerRank_Practice_Lab/Array_Analytics/permutationEquation.py
@@ -1,8 +1,21 @@
 
 def permutationEquation(a):
+    """
+    Solves the permutation equation by finding the index of the index of each value.
+    
+    Args:
+        a (list[int]): A list of n integers where each integer from 1 to n appears exactly once.
+        
+    Returns:
+        list[int]: A list of integers representing the values of y for each x from 1 to n.
+    """
     return [a.index(a.index(a.index(i) + 1) + 1) + 1 for i in a]
 
 def main():
+    """
+    Handles input and output for the permutation equation solver.
+    Expects the number of elements followed by the space-separated elements of the permutation.
+    """
     n = int(input().strip())
     p = list(map(int, input().rstrip().split()))
 
