1b2b307e327ae2895df25e62327a6ff8d44ba322
AnamolZ <anmoldkl971@gmail.com>
2026-02-01 14:48:44 +0545

    feat: implement solution for the Chocolate Feast problem.

--- python/hackerrank/math/chocolateFeast.py
+++ python/hackerrank/math/chocolateFeast.py
@@ -1,16 +1,22 @@
 
 def chocolateFeast(n, c, m):
-    te = n // c
-    cw = te
-    while cw >= m:
-        nb = cw // m
-        te = te + nb
-        cw = nb + (cw % m)
-    return te
+    # Initial chocolates bought with money
+    chocolates = n // c
+    wrappers = chocolates
+    total_eaten = chocolates
+    
+    # Trade in wrappers for more chocolates
+    while wrappers >= m:
+        new_chocolates = wrappers // m
+        total_eaten += new_chocolates
+        # New wrappers = remainder from trade + wrappers from new chocolates
+        wrappers = (wrappers % m) + new_chocolates
+        
+    return total_eaten
 
 def main():
     t = int(input())
-    for _ in t:
+    for _ in range(t):
         n, c, m = map(int, input().rstrip().split())
         print(chocolateFeast(n, c, m))
 
