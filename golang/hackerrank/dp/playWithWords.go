52490b7cbb1c85c07ae7da18ad19607c230c70d4
AnamolZ <anmoldkl971@gmail.com>
2026-02-02 23:01:37 +0545

    feat: Implement dynamic programming solution for maximum product of non-overlapping palindromic subsequences.

---
+++ golang/hackerrank/dp/playWithWords.go
@@ -0,0 +1,48 @@
+package dp
+
+import (
+	"fmt"
+)
+
+// Solve calculates the maximum product of lengths of two non-overlapping palindromic subsequences.
+func Solve() {
+	s := "eeegeeksforskeeggeeks"
+	n := len(s)
+
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		dp[i][i] = 1
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	maxProduct := 0
+	for i := 0; i < n-1; i++ {
+		product := dp[0][i] * dp[i+1][n-1]
+		if product > maxProduct {
+			maxProduct = product
+		}
+	}
+
+	fmt.Println(maxProduct)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	Solve()
+}
