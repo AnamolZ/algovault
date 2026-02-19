0a2b6a4f8812e54833cec6145bd521a256907085
Anamol <anmoldkl971@gmail.com>
2026-02-20 03:24:24 +0545

    feat: implement solution for HackerRank Service Lane problem.

--- python/hackerrank/simulation/serviceLane.py
+++ python/hackerrank/simulation/serviceLane.py
@@ -1,5 +1,12 @@
 
 def serviceLane():
+    """
+    Solves the 'Service Lane' problem.
+    Reads the length of the freeway and the number of test cases,
+    then reads an array capturing the width of the service lane at each segment.
+    For each test case, reads entry and exit indices and prints the maximum 
+    vehicle width that can pass through that segment of the service lane.
+    """
     n, t = map(int, input().split())
     width = list(map(int, input().split()))
 
