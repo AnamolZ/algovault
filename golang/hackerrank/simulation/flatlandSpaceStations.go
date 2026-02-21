cb905cb075e0904de25b4bf4c1cbfc7e119a183a
Anamol <anmoldkl971@gmail.com>
2026-02-22 01:25:38 +0545

    feat: implement `FlatlandSpaceStations` function to calculate the maximum distance from any city to its nearest space station.

--- golang/hackerrank/simulation/flatlandSpaceStations.go
+++ golang/hackerrank/simulation/flatlandSpaceStations.go
@@ -4,6 +4,19 @@ import (
 	"sort"
 )
 
+/*
+FlatlandSpaceStations calculates the maximum distance from any city to its nearest space station.
+
+Args:
+
+	n: The number of cities.
+	m: The number of space stations.
+	c: An integer slice of the indices of cities that have a space station.
+
+Returns:
+
+	int: The maximum distance any city is from a space station.
+*/
 func FlatlandSpaceStations(n int, m int, c []int) int {
 	sort.Ints(c)
 
