fd180f88382f10aad0c72edec16b61ba65a8ee5d
Anamol <anmoldkl971@gmail.com>
2026-02-22 01:10:40 +0545

    feat: add Workbook function to calculate the number of special problems in a workbook.

--- golang/hackerrank/simulation/workbook.go
+++ golang/hackerrank/simulation/workbook.go
@@ -1,5 +1,18 @@
 package simulation
 
+/*
+Workbook calculates the number of special problems in Lisa's workbook.
+A problem is considered "special" if its index (within a chapter)
+is the same as the page number it's located on.
+
+Args:
+    n: The number of chapters.
+    k: The maximum number of problems per page.
+    arr: An integer array where arr[i] denotes the number of problems in the (i+1)-th chapter.
+
+Returns:
+    int: The total number of special problems in the workbook.
+*/
 func Workbook(n int, k int, arr []int) int {
 	page := 1
 	special := 0
